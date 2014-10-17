c1745499ea5ee35352b1cbc6c5931c3615000e81
German Ostaszynski <german92@gmail.com>
2014-10-17 10:14:04 -0400

    Add IntroSql

---
+++ IntroSql/BookSchema.sql
@@ -0,0 +1,10 @@
+insert into customers values
+  (NULL, "Julie Smith", "25 Oak Street", "Airport West"),
+  (NULL, "Alan Wong", "1/47 Haines Avenue", "Box Hill"),
+  (NULL, "Michelle Arthur", "357 North Road", "Yarraville");
+
+insert into orders values
+  (NULL, 3, 69.98, "2000-04-02"),
+  (NULL, 1, 49.99, "2000-04-15"),
+  (NULL, 2, 74.98, "2000-04-19"),
+  (NULL, 3, 24.99, "2000-05-01");
